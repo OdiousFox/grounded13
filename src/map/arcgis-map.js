ac5aa866823810cd043cea611f3734615ee19cef
OdiousFox <goombajames@yahoo.com>
2024-05-28 17:35:10 +0200

    v5: added new toggleable layers, legend and searchbar

--- src/map/arcgis-map.js
+++ src/map/arcgis-map.js
@@ -1,23 +1,22 @@
-import React, { Component } from "react";
-import { createRoot } from "react-dom/client";  // Import createRoot
-import {Map, WebMap} from "@esri/react-arcgis";
-
+import React, { useState } from "react";
+import { Map, WebMap } from "@esri/react-arcgis";
 import "./arcgis-map.css";
 
-class ArcgisMap extends Component {
-    render() {
-        return (
-            // <Map className={"map-container"}
-            //     mapProperties={{ basemap: "streets-vector" }}
-            //     viewProperties={{ center: [-90, 38] }}
-            //     loaderOptions={{ version: "4.13", css: true }}
-            // />
-        <WebMap className={"map-container"}
-                id="c711e980dcae489297c9646fc686a8c5"  // Replace with your WebMap ID
-                loaderOptions={{ version: "4.13", css: true }}
-        />
-        );
-    }
-}
+const ArcgisMap = () => {
+    const [appUrl, setAppUrl] = useState("https://experience.arcgis.com/experience/e0855e0eeb8d4e4abd4e742e8f6a922a/"); // default map ID
+
+    return (
+        <div className="map-container">
+            <iframe
+                src={appUrl}
+                width="100%"
+                height="100%"
+                frameBorder="0"
+                allowFullScreen
+                title="ArcGIS Experience Builder"
+            ></iframe>
+        </div>
+    );
+};
 
 export default ArcgisMap;
